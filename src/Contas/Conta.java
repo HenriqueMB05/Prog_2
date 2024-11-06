2eb4a63c883d0b00a80283e13ddfc6f45a9b4126
Henrique <matosh38@gmail.com>
2024-11-06 08:28:36 -0300

    Criando o cliente e modificando a conta

--- src/Contas/Conta.java
+++ src/Contas/Conta.java
@@ -8,30 +8,40 @@ public class Conta {
 	public Integer numero;
 	public Double saldo;
 	public Double limite;
-	public String nome;
+	public Cliente titular;
 	
-	public Conta(Integer numero, Double saldo, Double limite, String nome) {
+	public Conta(Integer numero, Double saldo, Double limite, Cliente titular) {
 		this.numero = numero;
 		this.saldo = saldo;
 		this.limite =limite;
-		this.nome = nome;
+		this.titular = titular;
+	}
+	
+	public Conta(Integer numero, Double limite, Cliente titular) {
+		this.numero = numero;
+		this.titular= titular;
+		this.saldo = 0.0;
+		this.limite = limite;
 	}
 	
 	public Boolean saca(Double valor) {
-		if(valor<=this.saldo) {		
-			this.saldo = this.saldo-valor;
-			System.out.println("Saque realizado");
-			return true;
-		}
-		System.out.println("Não foi possivel sacar");
+			if(valor<=this.saldo && valor>0) {		
+				this.saldo = this.saldo-valor;
+				return true;
+			}
+		System.out.println("Valor informado para saque inválido!");	
 		return false;
 	
 	}
 	
 	public void deposita(Double valor) {
-		this.saldo = this.saldo+valor;
-	}
-	
+		if(valor>0) {
+			this.saldo = this.saldo+valor;
+		} else {
+			System.out.println("Valor informado para deposito inválido!");
+			return;
+		}
+	}	
 	
 
 }
--- src/Contas/Program.java
+++ src/Contas/Program.java
@@ -2,11 +2,29 @@ package Contas;
 
 public class Program {
 	public static void main(String[] args) {
-		Conta c1 = new Conta(1234, 1200.00, 30000.00, "Henrique");
+		Cliente t1 = new Cliente ("Henrique", "Bonfim", "000000000-00");
+		Conta c1 = new Conta(1234, 30000.0, t1);
 		
+		
+		System.out.printf("Titular: %s %s\n", c1.titular.nome, c1.titular.sobrenome);
+		System.out.printf("Número da conta: %d\n", c1.numero);
+		System.out.printf("Saldo da conta: R$%.2f\n", c1.saldo);
+		System.out.printf("Limite da conta: R$%.2f\n", c1.limite);
+		
+		System.out.println();
+		
+		c1.deposita(10000.00);
+		System.out.println("Depositando o valor de R$10.000,00");
+		System.out.printf("Saldo da conta: R$%.2f\n", c1.saldo);
 		
 		c1.saca(1200.00);
-		c1.deposita(10000.00);
-		System.out.println(c1.saldo);
+		System.out.println("Sacando o valor de R$1.200,00");
+		System.out.printf("Saldo da conta: R$%.2f\n", c1.saldo);
+		
+		System.out.println();
+		
+		System.out.println("CPF do titular: "+ c1.titular.cpf);
+		
+
 	}
 }
